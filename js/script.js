ce1cfe44796910a8cd58f7b3d86df432f1f73bbc
BetterCallAle <dap22091998@gmail.com>
2022-10-12 16:09:36 +0200

    exported the 2 buttons

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 //create a array with img names
 const imgArray = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
-console.log(imgArray);
+
 
 //export the div "item" from DOM
 const container = document.querySelector(".item")
-console.log(container);
+
 
 
 //create 5 div elements with for cycle
@@ -16,12 +16,16 @@ for(let i = 0; i < imgArray.length; i++){
     imgItem.classList.add("hidden")
     
     container.append(imgItem)
-    console.log(container);
 
     const imgElement = `<img src="${thisImg}" alt="Immagine del carosello">`
     
     imgItem.innerHTML = imgElement
-    console.log(imgItem);
 }
 
+//export the 5 div created before
+const imgItemExported = document.getElementsByClassName("img-item")
+console.log(imgItemExported);
 
+//export the 2 buttons
+const nextBtn = document.querySelector(".btn-down")
+const prevBtn = document.querySelector(".btn-up")
