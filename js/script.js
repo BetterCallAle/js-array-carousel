0d1bae83623db3a0e01957a3030a6bc65db0eebd
BetterCallAle <dap22091998@gmail.com>
2022-10-12 16:02:48 +0200

    change the position of export div 'item'

--- js/script.js
+++ js/script.js
@@ -2,15 +2,26 @@
 const imgArray = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
 console.log(imgArray);
 
-//create 5 elements in the DOM with for cycle
+//export the div "item" from DOM
+const container = document.querySelector(".item")
+console.log(container);
+
+
+//create 5 div elements with for cycle
 for(let i = 0; i < imgArray.length; i++){
     const thisImg = imgArray[i];
 
     const imgItem = document.createElement("div");
     imgItem.classList.add("img-item");
+    imgItem.classList.add("hidden")
+    
+    container.append(imgItem)
+    console.log(container);
 
     const imgElement = `<img src="${thisImg}" alt="Immagine del carosello">`
     
     imgItem.innerHTML = imgElement
     console.log(imgItem);
 }
+
+
